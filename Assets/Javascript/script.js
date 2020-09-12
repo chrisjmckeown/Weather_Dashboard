4a3b5d33c673699b74e67c2ed72147a695d5641f
Chris Mckeown <chris.j.mckeown@hotmail.com>
2020-09-13 00:07:52 +1000

    code refinement and style adjustments to css

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -1,18 +1,24 @@
 $(document).ready(function () {
 
     // OpenWeatherMap API key
-    var OpenWeatherMapAPIKey = "9bf53c7d1a1caa6883ae1bf798a2a10c"; //53b3da334ab80c9261f53586ac885ee0 //166a433c57516f51dfab1f7edaed8413
+    var OpenWeatherMapAPIKey = "53b3da334ab80c9261f53586ac885ee0"; //53b3da334ab80c9261f53586ac885ee0 //166a433c57516f51dfab1f7edaed8413
     // ipinfo API key
     var ipinfoAPIKey = "f2357f3657a5f4";
     // search list and default variables
     var recentSearchList = [];
-    var defaultCity = "Auckland, NZ"; // set Auckland, NZ as default incase location access is not granted
+    var defaultCity = "Auckland, NZ"; // set Auckland, NZ as default incase ipinfo.io access is not granted
     getDefaultCityCountry();
 
     // use ipinfo to source the local city and country
     function getDefaultCityCountry() {
-        var query = "https://ipinfo.io?token=" + ipinfoAPIKey;
-        $.ajax({ url: query, method: 'get' }).then(function (response) {
+        // Create an AJAX call to retrieve data Log the data in console
+        var queryParameters = {
+            token: ipinfoAPIKey,
+        };
+        var queryString = $.param(queryParameters);
+        var queryURL = "https://ipinfo.io?" + queryString;
+        // Call with a get method
+        $.ajax({ url: queryURL, method: 'get' }).then(function (response) {
             defaultCity = response.city + ", " + response.country;
         }).catch(function (err) {
             console.log(err);
@@ -23,29 +29,50 @@ $(document).ready(function () {
 
     // load/render the page
     function loadPage() {
-        // clear the page
-        reset5DayForecast();
         // get the stored search list
         getStoredSearchList();
-        // add to the UL
-        //renderSearchList();
+        // if the list is empty then find the default city, else...
         if (recentSearchList.length === 0 || recentSearchList === null) {
-            // if the list is empty then find the default city
             // display current weather
-            currentWeather(defaultCity);
+            getTodaysConditions(defaultCity);
             // display the 5 day forecast
-            fiveDayForecast(defaultCity);
+            getFiveDayForecast(defaultCity);
         }
-        else {
-            // find the last city in the list, last searched item
+        else { // find the last city in the list, the last searched item
             // display current weather
-            currentWeather(recentSearchList[recentSearchList.length - 1]);
+            getTodaysConditions(recentSearchList[recentSearchList.length - 1]);
             // display the 5 day forecast
-            fiveDayForecast(recentSearchList[recentSearchList.length - 1]);
+            getFiveDayForecast(recentSearchList[recentSearchList.length - 1]);
         }
     };
 
-    // both loadPage and currentWeather will call this, to save re-rendering the page
+    // Get the List from local storage
+    function getStoredSearchList() {
+        var storedRecentSearchList = JSON.parse(localStorage.getItem("recentSearchList"));
+        // check contents, if not null set to variable to list else if null to empty
+        if (storedRecentSearchList !== null) {
+            recentSearchList = storedRecentSearchList;
+        }
+    }
+
+    // Set the List from local storage
+    function setStoredSearchList(city) {
+        // see if an existing entry exists
+        var foundCity = recentSearchList.find(x => x === city);
+        // if not found then create an new item, else...
+        if (foundCity === undefined) {
+            // Add new to the array, clear the input
+            recentSearchList.push(city);
+        }
+        else { // move the found item in the array
+            // move the to the top of the list
+            array_move(recentSearchList, recentSearchList.findIndex(x => x === city), recentSearchList.length - 1);
+        }
+        // save the local storage with new items
+        localStorage.setItem("recentSearchList", JSON.stringify(recentSearchList));
+    }
+
+    // display the search list, called after setStoredSearchList
     function renderSearchList() {
         $("#recent-search-list").empty();
         recentSearchList.forEach(function (city) {
@@ -56,39 +83,6 @@ $(document).ready(function () {
             $("#recent-search-list").prepend($cityItem);
         });
     };
-
-    function reset5DayForecast() {
-        $("#five-day-forecast").empty();
-    };
-
-    function getStoredSearchList() {
-        // Get calendarList from local storage
-        var storedRecentSearchList = JSON.parse(localStorage.getItem("recentSearchList"));
-        // check contents, if not null set to variable to list else if null to empty
-        if (storedRecentSearchList !== null) {
-            recentSearchList = storedRecentSearchList;
-        }
-    }
-
-    // saves the to localStorage
-    function setStoredSearchList(city) {
-        console.log(recentSearchList);
-        // see if an existing entry exists
-        var foundCity = recentSearchList.find(x => x === city);
-        // if found then move the existing in the array, else create an new
-        if (foundCity === undefined) {
-            // Add new to the array, clear the input
-            recentSearchList.push(city);
-        }
-        else {
-            // move the to the top of the list
-            array_move(recentSearchList, recentSearchList.findIndex(x => x === city), recentSearchList.length - 1);
-        }
-        // save the local storage with new items
-        localStorage.setItem("recentSearchList", JSON.stringify(recentSearchList));
-        //render the search list with recent search item at the top
-        renderSearchList();
-    }
 
     // function to reorder the array
     function array_move(arr, old_index, new_index) {
@@ -105,9 +99,9 @@ $(document).ready(function () {
     $(document.body).on('click', "#recent-searched-item", function (event) {
         event.preventDefault();
         // display current weather
-        currentWeather(this.textContent);
+        getTodaysConditions(this.textContent);
         // display the 5 day forecast
-        fiveDayForecast(this.textContent);
+        getFiveDayForecast(this.textContent);
     });
 
     // Get the city input and call function to find the city
@@ -115,9 +109,9 @@ $(document).ready(function () {
         event.preventDefault();
         var city = $("#city-input").val().trim();
         // display current weather
-        currentWeather(city);
+        getTodaysConditions(city);
         // display the 5 day forecast
-        fiveDayForecast(city);
+        getFiveDayForecast(city);
     });
 
     // clear the search history and 
@@ -126,13 +120,16 @@ $(document).ready(function () {
         // empty the search list and then add in a default city
         recentSearchList = [];
         // display current weather
-        currentWeather(defaultCity);
+        getTodaysConditions(defaultCity);
         // display the 5 day forecast
-        fiveDayForecast(defaultCity);
+        getFiveDayForecast(defaultCity);
+        // clear the input text
+        $("#city-input").val("");
     });
 
-    function currentWeather(city) {
-        // Create an AJAX call to retrieve data Log the data in console
+    // ajax query to get the todays conditions
+    function getTodaysConditions(city) {
+        // Create an AJAX call to retrieve data
         var queryParameters = {
             q: city,
             units: "metric",
@@ -144,35 +141,19 @@ $(document).ready(function () {
         $.ajax({ url: queryURL, method: 'get' }).then(function (response) {
             // if successfull then store the result
             setStoredSearchList(response.name + ", " + response.sys.country);
+            //render the search list with recent search item at the top
+            renderSearchList();
             // display the conditions for the today
-            recordToday(response);
-        }).catch(function (err) {
-            console.log(err);
-        });
-    };
-
-    function fiveDayForecast(city) {
-        // Create an AJAX call to retrieve data Log the data in console
-        var queryParameters = {
-            q: city,
-            units: "metric",
-            appid: OpenWeatherMapAPIKey,
-        };
-        var queryString = $.param(queryParameters);
-        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + queryString;
-        // Call with a get method
-        $.ajax({ url: queryURL, method: 'get' }).then(function (response) {
-            // reset the div containing the 5 day forcast
-            reset5DayForecast();
-            // display the conditions for the next 5 days
-            loopResponse(response.list, date);
+            renderTodaysConditions(response);
+            // clear the input text
+            $("#city-input").val("");
         }).catch(function (err) {
             console.log(err);
         });
     };
 
     // display todays conditions
-    function recordToday(response) {
+    function renderTodaysConditions(response) {
         // Populate the city and Country (with out the country it is hard to know which country the city is in)
         $("#city").text(response.name + " (" + response.sys.country + ")");
         // use the first item in the list for the first display
@@ -191,11 +172,32 @@ $(document).ready(function () {
         var lat = response.coord.lat;
         var lon = response.coord.lon;
 
-        setUV(lon, lat);
+        renderUV(lon, lat);
     }
 
+    // ajax query to get the five day forecast
+    function getFiveDayForecast(city) {
+        // Create an AJAX call to retrieve data Log the data in console
+        var queryParameters = {
+            q: city,
+            units: "metric",
+            appid: OpenWeatherMapAPIKey,
+        };
+        var queryString = $.param(queryParameters);
+        var queryURL = "https://api.openweathermap.org/data/2.5/forecast?" + queryString;
+        // Call with a get method
+        $.ajax({ url: queryURL, method: 'get' }).then(function (response) {
+            // reset the div containing the 5 day forcast
+            $("#five-day-forecast").empty();
+            // display the conditions for the next 5 days
+            extractDays(response.list, date);
+        }).catch(function (err) {
+            console.log(err);
+        });
+    };
+
     // Loop through an array, extracting the data at midday or the last item if there not 5 midday items in the record
-    function loopResponse(list, date) {
+    function extractDays(list, date) {
         // counter to check 5 days are displayed
         var count = 0;
         // iterate over each item
@@ -208,17 +210,18 @@ $(document).ready(function () {
             }
             // if the hour is midday then display the conditions. It looks like when the api is called, the result start at the next 3hr slot 
             if (hour === "12") {
-                createDay(item);
+                renderDay(item);
                 count++;
             }
         });
         // if there less than 5 days recorded then display the last conditions in the list
         if (count < 5) {
-            createDay(response.list[list.length - 1]);
+            renderDay(response.list[list.length - 1]);
         }
     };
 
-    function createDay(item) {
+    // display each day conditions
+    function renderDay(item) {
         // create a div to append all the data too
         var $day = $("<div>");
         // gather the data into variables
@@ -239,7 +242,8 @@ $(document).ready(function () {
         $("#five-day-forecast").append($day);
     };
 
-    function setUV(lon, lat) {
+    // color code the UV conditions
+    function renderUV(lon, lat) {
         // create the uv query
         var queryParameters = {
             appid: OpenWeatherMapAPIKey,
